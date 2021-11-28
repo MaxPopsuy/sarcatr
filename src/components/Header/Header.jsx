8cf4a65ed65841bca01a629157895d68d541799b
Max Popsuy <maxim09p@gmail.com>
2021-11-28 18:39:59 +0200

    home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './header.module.css';
+import styles from './header.module.scss';
 import { nanoid } from 'nanoid';
 
 const Header = (props) => {
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './home.module.css';
+import styles from './home.module.scss';
 // import { nanoid } from 'nanoid';
 
 const Home = (props) => {
@@ -7,7 +7,7 @@ const Home = (props) => {
     <section className={styles.home}> 
       <h2 className = {styles.home__title}>{props.data.title}</h2>
       <div>
-        <p>Sarcatr is universal site and programm with all do you need</p>
+        <p className={styles.home__text}>Sarcatr is universal site and programm with all do you need</p>
       </div>
     </section>
   );
